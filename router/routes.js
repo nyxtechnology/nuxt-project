841760a766b6d8665ed9bfb8aa8aeb57571ed37d
Alan Ktquez <ktquez@gmail.com>
2019-08-30 08:45:29 -0300

    Update routes to i18n

--- router/routes.js
+++ router/routes.js
@@ -3,7 +3,12 @@ import Home from '~/pages/Home'
 export default [
   {
     path: '/',
-    name: 'home',
+    name: 'home___en',
+    component: Home
+  },
+  {
+    path: '/pt',
+    name: 'home___pt',
     component: Home
   }
 ]
